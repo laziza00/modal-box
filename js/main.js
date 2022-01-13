801a4fb95d04e6e2e7a2ffaeff2b4b339a891aff
laziza@mail.ru <laziza00>
2022-01-13 12:06:22 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+
+let elmodalbox=document.getElementById('modalBox');
+let elmodalbtn=document.getElementById('ModalBtn');
+let elmodalclose=document.getElementById('closeBtn');
+
+elmodalbtn.addEventListener('click',function () {
+elmodalbox.classList.add('show');
+});
+
+elmodalclose.addEventListener('click',function () {
+elmodalbox.classList.remove('show');
+ });
+
+
+ elmodalbtn.addEventListener('click',function () {
+elmodalbox.classList.add('show');
+});
